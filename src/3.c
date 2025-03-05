0891a4e6136d5d73496f1f24c32623dc71b831c4
veliapg3g <veliapg3g@gmx.com>
2025-03-05 19:11:53 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int x = 5;
+    if(x > 0)
+    {
+        printf("X is greater than zero\n");
+    }
+    else if(x == 0)
+    {
+        printf("X is equal to zero\n");
+    }
+    else
+    {
+        printf("X is less than zero\n");
+    }
+    return 0;
+}
